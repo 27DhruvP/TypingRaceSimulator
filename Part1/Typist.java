59af16e7e6c612339c726406b2a5bf1f40fcf4fc
Dhruv Patel <2710dhruvpatel@gmail.com>
2026-04-21 20:22:44 +0100

    Added method isBurntOut in class Typist which returns true or false.

--- Part1/Typist.java
+++ Part1/Typist.java
@@ -156,7 +156,7 @@ public class Typist
      */
     public boolean isBurntOut()
     {
-        return false; // placeholder - replace with correct implementation
+        return isBurntOut;
     }
 
     /**
