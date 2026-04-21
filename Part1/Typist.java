eac5267645d59fcf21634ae717d6c58e5e88be39
Dhruv Patel <2710dhruvpatel@gmail.com>
2026-04-21 20:27:47 +0100

    Added method newAccuracy to class Typist which sets the accuracy rating of the typist

--- Part1/Typist.java
+++ Part1/Typist.java
@@ -198,6 +198,18 @@ public class Typist
      */
     public void setAccuracy(double newAccuracy)
     {
+        if (newAccuracy < 0.0)
+        {
+            accuracy = 0.0;
+        }
+        else if (newAccuracy > 1.0)
+        {
+            accuracy = 1.0;
+        }
+        else
+        {
+            accuracy = newAccuracy;
+        }
 
     }
 
@@ -208,6 +220,7 @@ public class Typist
      */
     public void setSymbol(char newSymbol)
     {
+        symbol = newSymbol;
 
     }
 
