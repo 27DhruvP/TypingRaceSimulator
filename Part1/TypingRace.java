9fffd8ff775c178e9abb828bc6d89fe3c38f50fe
Dhruv Patel <2710dhruvpatel@gmail.com>
2026-04-23 21:51:25 +0100

    Added a main class to the code

--- Part1/TypingRace.java
+++ Part1/TypingRace.java
@@ -32,6 +32,17 @@ public class TypingRace
      *
      * @param passageLength the number of characters in the passage to type
      */
+
+
+
+    public static void main(String[] args) {
+        TypingRace race = new TypingRace(40);
+        race.addTypist(new Typist('①', "TURBOFINGERS", 0.85), 1);
+        race.addTypist(new Typist('②', "QWERTY_QUEEN",  0.60), 2);
+        race.addTypist(new Typist('③', "HUNT_N_PECK",   0.30), 3);
+        race.startRace();
+    }
+    
     public TypingRace(int passageLength)
     {
         this.passageLength = passageLength;
